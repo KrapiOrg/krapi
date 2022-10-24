7679a49f680ef7a5aa993fea9f4b66db137f7deb
hashem78 <hashem.olayano@gmail.com>
2022-10-24 16:05:27 +0300

    Add transaction model

---
+++ src/SharedModels/Transaction.h
@@ -0,0 +1,30 @@
+//
+// Created by mythi on 24/10/22.
+//
+
+#ifndef SHARED_MODELS_TRANSACTION_H
+#define SHARED_MODELS_TRANSACTION_H
+
+#include "nlohmann/json.hpp"
+
+namespace krapi {
+
+    enum class TransactionType {
+        Send
+    };
+
+    NLOHMANN_JSON_SERIALIZE_ENUM(TransactionType, {
+        { TransactionType::Send, "send_tx" },
+    })
+
+    struct Transaction {
+        TransactionType type;
+        int from;
+        int to;
+
+        NLOHMANN_DEFINE_TYPE_INTRUSIVE(Transaction, type, from, to)
+    };
+
+} // krapi
+
+#endif //SHARED_MODELS_TRANSACTION_H
