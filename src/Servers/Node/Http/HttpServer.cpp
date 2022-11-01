bdfac8a02ac0e8d7e43df12d259153487a802690
hashem78 <hashem.olayano@gmail.com>
2022-11-01 18:57:10 +0300

    Split HttpServer to definition and implementation

--- src/Servers/Node/Http/HttpServer.cpp
+++ src/Servers/Node/Http/HttpServer.cpp
@@ -5,4 +5,86 @@
 #include "HttpServer.h"
 
 namespace krapi {
+    HttpServer::HttpServer(ServerHost host, MessageQueuePtr mq, TransactionQueuePtr tq,
+                           std::shared_ptr<IdentityManager> identity_manager) :
+            m_host(std::move(host)),
+            m_node_message_queue(std::move(mq)),
+            m_tx_queue(std::move(tq)),
+            m_identity_manager(std::move(identity_manager)),
+            m_server(m_host.second, m_host.first) {
+
+        setup_listeners();
+    }
+
+    void HttpServer::start() {
+
+        m_internal_queue.dispatch(HttpServerInternalMessage::Start);
+    }
+
+    void HttpServer::wait() {
+
+        m_internal_queue.dispatch(HttpServerInternalMessage::Block);
+    }
+
+    void HttpServer::stop() {
+
+        m_internal_queue.dispatch(HttpServerInternalMessage::Stop);
+    }
+
+    HttpServer::~HttpServer() {
+
+        stop();
+
+    }
+
+    void HttpServer::setup_listeners() {
+
+
+        m_internal_queue.appendListener(HttpServerInternalMessage::Start, [this]() {
+            auto res = m_server.listen();
+            if (!res.first) {
+                spdlog::error("{}", res.second);
+                exit(1);
+            }
+            m_server.start();
+            spdlog::info("Started http server");
+        });
+
+        m_internal_queue.appendListener(HttpServerInternalMessage::Block, [this]() {
+            m_server.wait();
+        });
+
+        m_internal_queue.appendListener(HttpServerInternalMessage::Stop, [this]() {
+            m_server.stop();
+        });
+
+        m_server.setOnConnectionCallback(
+                [this](
+                        const ix::HttpRequestPtr &req,
+                        const std::shared_ptr<ix::ConnectionState> &state
+                ) -> ix::HttpResponsePtr {
+                    if (req->method == "POST") {
+                        auto msg_json = nlohmann::json::parse(req->body);
+                        auto msg = msg_json.get<krapi::HttpMessage>();
+                        if (msg.type == krapi::NodeHttpMessageType::AddTx) {
+                            spdlog::info("HttpServer recieved addtx message");
+                            auto tx = msg.content.get<krapi::Transaction>();
+                            m_tx_queue->dispatch(0, tx);
+                        }
+                        return std::make_shared<ix::HttpResponse>();
+                    } else {
+                        auto header = ix::WebSocketHttpHeaders();
+                        header["Content-Type"] = "application/json";
+                        return std::make_shared<ix::HttpResponse>(
+                                200,
+                                "OK",
+                                ix::HttpErrorCode::Ok,
+                                header,
+                                nlohmann::json(m_identity_manager->identity()).dump()
+                        );
+                    }
+
+                }
+        );
+    }
 } // krapi
--- src/Servers/Node/Http/HttpServer.h
+++ src/Servers/Node/Http/HttpServer.h
@@ -38,56 +38,7 @@ namespace krapi {
         ix::HttpServer m_server;
 
 
-        void setup_listeners() {
-
-
-            m_internal_queue.appendListener(HttpServerInternalMessage::Start, [this]() {
-                auto res = m_server.listen();
-                if (!res.first) {
-                    spdlog::error("{}", res.second);
-                    exit(1);
-                }
-                m_server.start();
-                spdlog::info("Started http server");
-            });
-
-            m_internal_queue.appendListener(HttpServerInternalMessage::Block, [this]() {
-                m_server.wait();
-            });
-
-            m_internal_queue.appendListener(HttpServerInternalMessage::Stop, [this]() {
-                m_server.stop();
-            });
-
-            m_server.setOnConnectionCallback(
-                    [this](
-                            const ix::HttpRequestPtr &req,
-                            const std::shared_ptr<ix::ConnectionState> &state
-                    ) -> ix::HttpResponsePtr {
-                        if (req->method == "POST") {
-                            auto msg_json = nlohmann::json::parse(req->body);
-                            auto msg = msg_json.get<krapi::HttpMessage>();
-                            if (msg.type == krapi::NodeHttpMessageType::AddTx) {
-                                spdlog::info("HttpServer recieved addtx message");
-                                auto tx = msg.content.get<krapi::Transaction>();
-                                m_tx_queue->dispatch(0, tx);
-                            }
-                            return std::make_shared<ix::HttpResponse>();
-                        } else {
-                            auto header = ix::WebSocketHttpHeaders();
-                            header["Content-Type"] = "application/json";
-                            return std::make_shared<ix::HttpResponse>(
-                                    200,
-                                    "OK",
-                                    ix::HttpErrorCode::Ok,
-                                    header,
-                                    nlohmann::json(m_identity_manager->identity()).dump()
-                            );
-                        }
-
-                    }
-            );
-        }
+        void setup_listeners();
 
     public:
 
@@ -96,36 +47,15 @@ namespace krapi {
                 MessageQueuePtr mq,
                 TransactionQueuePtr tq,
                 std::shared_ptr<IdentityManager> identity_manager
-        ) :
-                m_host(std::move(host)),
-                m_node_message_queue(std::move(mq)),
-                m_tx_queue(std::move(tq)),
-                m_identity_manager(std::move(identity_manager)),
-                m_server(m_host.second, m_host.first) {
+        );
 
-            setup_listeners();
-        }
+        void start();
 
-        void start() {
+        void wait();
 
-            m_internal_queue.dispatch(HttpServerInternalMessage::Start);
-        }
+        void stop();
 
-        void wait() {
-
-            m_internal_queue.dispatch(HttpServerInternalMessage::Block);
-        }
-
-        void stop() {
-
-            m_internal_queue.dispatch(HttpServerInternalMessage::Stop);
-        }
-
-        ~HttpServer() {
-
-            stop();
-
-        }
+        ~HttpServer();
     };
 
 } // krapi
