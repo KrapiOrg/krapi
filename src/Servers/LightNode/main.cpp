bcbbab635e966ba44962135426b25165d0f8641c
hashem78 <hashem.olayano@gmail.com>
2022-11-18 00:24:13 +0300

    Add randomness to the transactions lighnodes send.
    This commit also adds effolkronium's random library

--- src/Servers/LightNode/main.cpp
+++ src/Servers/LightNode/main.cpp
@@ -1,11 +1,13 @@
 #include <chrono>
 #include "LightNodeManager.h"
 #include "cryptopp/sha.h"
+#include "effolkronium/random.hpp"
 
 using namespace krapi;
 using namespace CryptoPP;
 using namespace std::chrono;
 using namespace std::chrono_literals;
+using Random = effolkronium::random_local;
 
 int main() {
 
@@ -17,13 +19,18 @@ int main() {
 
     std::thread thread(
             [&]() {
+                auto random = Random();
                 while (true) {
                     spdlog::info("Main: Sending TX#{}", message_count);
                     auto timestamp = (uint64_t) duration_cast<milliseconds>(
                             system_clock::now().time_since_epoch()).count();
                     std::string tx_hash;
                     StringSource s(
-                            fmt::format("{}{}{}{}{}{}", "send_tx", "tx_status_pending", timestamp, 0, 1, message_count),
+                            fmt::format(
+                                    "{}{}{}{}{}{}", "send_tx", "tx_status_pending",
+                                    timestamp, 0, 1,
+                                    random.get(1, 9999)
+                            ),
                             true,
                             new HashFilter(sha_256, new HexEncoder(new StringSink(tx_hash)))
                     );
