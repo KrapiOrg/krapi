1d54e6ac24b0a6a3faf30aa5aab2819dbd826f83
hashem78 <hashem.olayano@gmail.com>
2022-11-20 00:17:36 +0300

    Remove need for manual input in lightnode

--- src/Servers/LightNode/main.cpp
+++ src/Servers/LightNode/main.cpp
@@ -71,7 +71,7 @@ int main() {
                 [&]() {
 
                     while (true) {
-                        std::cin.get();
+
                         auto random_receiver = node_manager.get_random_light_node();
 
                         if (!random_receiver.has_value()) {
